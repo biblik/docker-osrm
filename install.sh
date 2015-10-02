46e684e2f3028c8879ec46a1f66a431f7c226e54
biblik <axel.grimault@gmail.com>
2015-10-02 11:36:10 +0200

    Add files

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# created by biblik<axel.grimault@gmail.com>
+
+export PROFILE_LUA=$(find /opt/osrm/profile/ -type f -name '*.lua')
+export DATA_OSM_PBF=$(find /opt/osrm/data/ -type f -name '*.pbf')
+
+echo $PROFILE_LUA
+echo $DATA_OSM_PBF
+
+cd /opt/osrm/osrm-backend/build
+
+ln -s $PROFILE_LUA profile.lua
+ln -s ../profiles/lib/
+
+./osrm-extract $DATA_OSM_PBF
+
+export DATA_OSM_OSRM=$(find /opt/osrm/data/ -type f -name '*.osrm')
+
+./osrm-prepare $DATA_OSM_OSRM
+
+./osrm-routed $DATA_OSM_OSRM --max-table-size=1000
